f10a6925681173a739d9f3bc3f524765ac175ec5
Fabián Huertas <miloh9318@hotmail.com>
2024-02-29 11:33:18 -0500

    creando primeras varaibles y funciones

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('lista-carrito tbody');
+const vaciarCarrito = document.querySelector('#vaciar-carrito');    
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    // Cuando agregas un curso presionando presionando "agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// Funciones
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        console.log(e.target);
+    };
+}
