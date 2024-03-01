2125f1724b337fff16998774edd793f1ae6a4b7c
Fabián Huertas <miloh9318@hotmail.com>
2024-03-01 18:05:11 -0500

    mostrando en el carrito el curso seleccionado

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');    
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -15,7 +16,7 @@ function cargarEventListeners() {
 
 function agregarCurso(e) {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')) {
+    if(e.target.classList.contains('agregar-carrito') ) {
         const cursoSeleccionado = e.target.parentElement.parentElement;
         leerDatosCurso(cursoSeleccionado);
     };
@@ -24,7 +25,7 @@ function agregarCurso(e) {
 //lee el contenido del html al que le dimos click  y extrae la informacion del curso
 
 function leerDatosCurso(curso) {
-    console.log(curso);
+    // console.log(curso);
 
     //Crear un objeto con el contenido del curso actual
     const infoCurso = {
@@ -35,5 +36,43 @@ function leerDatosCurso(curso) {
         cantidad: 1 
     }
 
-    console.log(infoCurso); 
+    // Agrega elementos al arreglo de carrito 
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    
+    console.log(articulosCarrito);
+
+    carritoHTML();
 }
+
+// Muestra el carrito de compras en el HTML
+
+function carritoHTML() {
+
+    // Limpiar el HTML
+
+    // limpiarHTML();
+
+    // Recore el carrito y genera el HTML
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        // Agrega el html del carrito en el tbody
+         contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elimina los cursos del tbody
+
+ function limpiarHTML () {
+    /* Forma lenta
+    contenedorCarrito.innerHTML = ''; */
+
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+} 
