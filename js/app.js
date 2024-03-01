0aea4ce8955bf0725a2353bf9b15884469becda5
Fabián Huertas <miloh9318@hotmail.com>
2024-03-01 16:30:35 -0500

    Leer los datos del curso que seleccionamos

--- js/app.js
+++ js/app.js
@@ -16,6 +16,24 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     };
+}
+
+//lee el contenido del html al que le dimos click  y extrae la informacion del curso
+
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent, 
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),  
+        cantidad: 1 
+    }
+
+    console.log(infoCurso); 
 }
