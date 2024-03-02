6bbf3059e31753ff6ee53d68639c3ff9c9092e69
Fabián Huertas <miloh9318@hotmail.com>
2024-03-01 19:10:46 -0500

    agregando un # que faltaba en lista-carrito

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // variables
 
 const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('lista-carrito tbody');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');    
 const listaCursos = document.querySelector('#lista-cursos');
 let articulosCarrito = [];
